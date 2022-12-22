4dc0b874c493071a3eced54aca61c06cdb4b6434
Ricardo Torres <tmricardo7@gmail.com>
2022-12-22 15:00:20 -0600

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    .myClass{
+      color: white;
+    }
+  `]
 })
 export class AppComponent {
   title = 'my-first-angular';
+  counter = 1;
+  showingData = false;
+  logs = [];
+
+  showData(){
+    this.showingData = true;
+    this.logs.push(`Counter = ${(this.counter++).toString()}`)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ListComponent } from './recipe/list.component';
+import { ListItemComponent } from './recipe/list-item.component';
+import { DetailComponent } from './recipe/detail.component';
+import { ListEditComponent } from './shopping/list-edit.component';
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ListItemComponent,
+    DetailComponent,
+    ListEditComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
